52db3659c7cf8271107c90c79bb8528152cfdbe1
Guilherme Lima <guilhermelima@MacBook-Pro-de-Guilherme.local>
2018-07-17 13:09:00 -0300

    codigo final

--- robo js/sketch.js
+++ robo js/sketch.js
@@ -1,108 +1,106 @@
-let fundo
-let robo
-let balao
-let resposta
-let numeroPergunta = 0
-let nome = ""
-let respostaTexto
+// imagens
+let imagemDoFundo;
+let imagemDoRobo;
+let imagemDoBalao;
 
+//análise e respostas
+let campoDeResposta;
+let numeroPergunta = 0;
+let nome = '';
+
+//carregando as imagens
 function preload() {
-  fundo = loadImage("fundo.png")
-  robo = loadImage("robo.png")
-  balao = loadImage("balao.png")
+  imagemDoFundo = loadImage("fundo.png");
+  imagemDoRobo = loadImage("robo.png");
+  imageDoBalao = loadImage("balao.png");
 }
 
+//quando ligar o jogo
 function setup() {
-  createCanvas(600, 400)
-  noLoop()
-  resposta = createInput();
+  createCanvas(600, 400);
+  noLoop();
+  campoDeResposta = createInput();
 }
 
 function draw() {
-  background(fundo)
-  image(robo, 150, 200, 150, 150)
-  image(balao, 80, 10, 300, 200)
-  textSize(15)
-  textAlign(CENTER)
-  textStyle(BOLD)
+  incluirImagens();
+  textSize(15);
+  textAlign(CENTER);
+  textStyle(BOLD);
   text(`Seja bem vindo ao Alura Genius!
 Para adivinhar seu nome, responda
 minhas perguntas com frases 
 longas.
-Clique na tela para começar`, 223, 50)
+Clique na tela para começar`, 290, 50);
+}
+
+function incluirImagens() {
+  background(imagemDoFundo);
+  image(imagemDoRobo, 220, 200, 150, 150);
+  image(imageDoBalao, 145, 10, 300, 200);
 }
 
 function mousePressed() {
   if (numeroPergunta == 0) {
-    numeroPergunta++
-    perguntar()
-    console.log('já clicou')
-    console.log(numeroPergunta)
+    numeroPergunta++;
+    perguntar();
   }
-
 }
 
-function desenharCena(texto){
- background(fundo)
-  image(robo, 150, 200, 150, 150)
-  image(balao, 80, 10, 300, 200)
-  textSize(30)
-  textAlign(CENTER)
-  textStyle(BOLD)
-  text(texto, 223, 70)
-  resposta.size(400, 20)
-  resposta.position(50, 360)
-  resposta.elt.placeholder = ('RESPONDA AQUI E PRESSIONE ENTER')
-  resposta.value('')
+function desenharCena(texto) {
+  incluirImagens();
+  textSize(30);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  text(texto, 285, 70);
+  caixaDeResposta();
+}
+
+//adicionar caixa de resposta
+function caixaDeResposta() {
+  campoDeResposta.size(450, 20);
+  campoDeResposta.position(85, 360);
+  campoDeResposta.elt.placeholder = ('RESPONDA AQUI E PRESSIONE ENTER');
+  campoDeResposta.value('');
 }
 
 function perguntar() {
- 	if (numeroPergunta == 1){ 
-    desenharCena("Qual seu filme\n preferido?")
-  }
-  else if (numeroPergunta == 2) {
-  	desenharCena("Qual seu livro\n preferido?")
-  }
-   else if (numeroPergunta == 3) {
-  	desenharCena("Qual sua fruta\n preferida?")
-   }
-  else{
-  desenharCena('Seu nome é ')
-  revelarNome()
+  if (numeroPergunta == 1) {
+    desenharCena('Qual seu filme\n preferido?');
+  } else if (numeroPergunta == 2) {
+    desenharCena("Qual seu livro\n preferido?");
+  } else if (numeroPergunta == 3) {
+    desenharCena("Qual sua fruta\n preferida?");
+  } else {
+    desenharCena('Seu nome é ');
+    revelarNome();
   }
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
-    if (numeroPergunta <= 3) {
-    descobrirNome()
+    if (numeroPergunta <= 3 && campoDeResposta.value() != '') {
+      descobrirNome();
     }
-    else{}
   }
 }
 
-function descobrirNome(){
-	respostaTexto = resposta.value()
-  if (respostaTexto.includes('.')) 
-  	{console.log('tem ponto')}
-  else{
-    nome = nome + respostaTexto[0]
-    console.log(nome)
+function descobrirNome() {
+  let respostaTexto;
+  respostaTexto = campoDeResposta.value();
+  if (verificarPonto(respostaTexto)) {
+    nome = nome + respostaTexto[0];
   }
-  numeroPergunta ++
-    console.log(numeroPergunta)
-    perguntar()
+  numeroPergunta++;
+  perguntar();
 }
 
-function revelarNome(){
-	resposta.remove()
+function verificarPonto(resposta) {
+  return !resposta.includes('.')
+}
+
+function revelarNome() {
+  campoDeResposta.remove()
   textSize(50)
-  text(nome, 223, 123) 
+  text(nome, 280, 123)
 }
-
-
-
-
-
-
-
